51b6bdf0a8b74f061694ae6240aab1c581e59c6e
Cody Bennett <hi@codyb.co>
2022-05-09 12:31:10 -0500

    Add `auto` for implicit pipeline layout (#96)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -733,7 +733,7 @@ interface GPUOrigin3DDict {
 
 interface GPUPipelineDescriptorBase
   extends GPUObjectDescriptorBase {
-  layout?: GPUPipelineLayout;
+  layout?: GPUPipelineLayout | 'auto';
 }
 
 interface GPUPipelineLayoutDescriptor
