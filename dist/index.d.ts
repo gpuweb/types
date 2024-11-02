d25aa0ed159606bd5498848700db08f316f07744
Kai Ninomiya <kainino@chromium.org>
2024-11-01 21:20:54 -0700

    format

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3217,14 +3217,20 @@ declare var GPUTextureUsage: {
 };
 
 /** @deprecated Use {@link GPUTexelCopyBufferLayout} */
-type GPUImageDataLayout = GPUTexelCopyBufferLayout;
+type GPUImageDataLayout =
+  GPUTexelCopyBufferLayout;
 /** @deprecated Use {@link GPUTexelCopyBufferInfo} */
-type GPUImageCopyBuffer = GPUTexelCopyBufferInfo;
+type GPUImageCopyBuffer =
+  GPUTexelCopyBufferInfo;
 /** @deprecated Use {@link GPUTexelCopyTextureInfo} */
-type GPUImageCopyTexture = GPUTexelCopyTextureInfo;
+type GPUImageCopyTexture =
+  GPUTexelCopyTextureInfo;
 /** @deprecated Use {@link GPUCopyExternalImageDestInfo} */
-type GPUImageCopyTextureTagged = GPUCopyExternalImageDestInfo;
+type GPUImageCopyTextureTagged =
+  GPUCopyExternalImageDestInfo;
 /** @deprecated Use {@link GPUCopyExternalImageSourceInfo} */
-type GPUImageCopyExternalImage = GPUCopyExternalImageSourceInfo;
+type GPUImageCopyExternalImage =
+  GPUCopyExternalImageSourceInfo;
 /** @deprecated Use {@link GPUCopyExternalImageSource} */
-type GPUImageCopyExternalImageSource = GPUCopyExternalImageSource;
+type GPUImageCopyExternalImageSource =
+  GPUCopyExternalImageSource;
